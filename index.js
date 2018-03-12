cb5b3d7a2df822d13dcfb253d7c92ee94a5e7249
Jon Wilson <jontwilson@gmail.com>
2018-03-12 14:10:05 -0400

    console logs all the data for one car

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const CREDS = require('./creds');
 // var TYPES = require('tedious').TYPES;  
 // var Connection = require('tedious').Connection;  
 
-const aVin = '1C3CCCFB8GN194471'; //'2C4RC1GG3JR100527'; //2A8HR54199R612914
 
 
-async function run(aVin) {
+async function LogIn(){
+    const aVin = '1C3CCCFB8GN194471'; //'2C4RC1GG3JR100527'; //2A8HR54199R612914
     const browser = await puppeteer.launch({
         headless: false
     });
     const page = await browser.newPage();
+    await getACar(page,aVin);
+    browser.close();
+}
+
+async function getACar(page,aVin) {
+
 
     // Log In
     const USERNAME_SELECTOR = '#FormsEditField1';
@@ -40,31 +46,34 @@ async function run(aVin) {
     let numTableRows = await page.evaluate((sel) => {
         return document.querySelectorAll(sel).length;
       }, TABLE_ROW);
-    console.log('numTableRows',numTableRows);
 
     //radio boxes (tr:nth-child(INDEX) should start at 3)
     const DATE_RADIO_SELECTOR = '#searchCriteria > tbody > tr > td > table.dcTable > tbody > tr:nth-child(INDEX) > td:nth-child(1) > input[type="radio"]:nth-child(8)';
-    //Owner Status
-    const OWNER_STATUS = '#searchCriteria > tbody > tr > td > table.dcTable > tbody > tr:nth-child(INDEX) > td:nth-child(8)'; // "Current Owner","Service Customer","Dubious Owner"
-    //Tabs at top to go to address screen and back to list
+    //Table Columns
+    const TABLE_FIELDS = '#searchCriteria > tbody > tr > td > table.dcTable > tbody > tr:nth-child(ROWINDEX) > td:nth-child(COLINDEX)';
+
+    //Tabs at top to go to address screen, back to table, and back to select another VIN
     const ACTIVE_ADDRESS_TAB_SELECTOR = '#correctionsTab.anchorEnable  > nobr';
     const BACK_TO_LIST_TAB_SELECTOR = '#listTab > nobr';
+    const BACK_TO_CRITERIA_TAB_SELECTOR = '#criteriaTab > nobr';
     ////Address Screen
-    const EMAIL_FIELD_1 = '#searchCriteria > tbody > tr > td > table:nth-child(3) > tbody > tr > td:nth-child(2) > table:nth-child(1) > tbody > tr:nth-child(16) > td:nth-child(2) > input[type="text"]';
-    const EMAIL_FIELD_2 = '#searchCriteria > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(4) > td:nth-child(2) > input[type="text"]'; 
-    const ALLTEXTINPUT = 'input[type="text"]'; 
-
+    const TITLE_FIELD_DROPDOWN = '#searchCriteria > tbody > tr > td > table:nth-child(3) > tbody > tr > td:nth-child(2) > table:nth-child(1) > tbody > tr:nth-child(8) > td:nth-child(2) > select'; 
+    const TITLE_FIELD_TEXT = '#searchCriteria > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody > tr:nth-child(8) > td:nth-child(2) > table > tbody > tr > td'; 
     const OPT_IN_FLAG = '#searchCriteria > tbody > tr > td > table:nth-child(3) > tbody > tr > td:nth-child(2) > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(1) > input[type="radio"]:nth-child(1)';
 
     // document.querySelector('#searchCriteria > tbody > tr > td > table:nth-child(3) > tbody > tr > td:nth-child(2) > table:nth-child(1) > tbody > tr:nth-child(16) > td:nth-child(2) > input[type="text"]');    
     // document.querySelector('#searchCriteria > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(4) > td:nth-child(2) > input[type="text"]');    
 
     for (let i = 3; i <= numTableRows; i++) { 
-        let OwnerStatus = await page.evaluate((sel) => {
-            let element = document.querySelector(sel);
-            return element? element.innerHTML: null;
-          }, OWNER_STATUS.replace("INDEX", i));
-        console.log('OwnerStatus',OwnerStatus.replace('<br>','').replace(/\s/g,''));
+        // TABLE VALUES
+        for(let j = 5; j <= 13; j++){
+            let ColVal = await page.evaluate((sel) => {
+                let element = document.querySelector(sel);
+                return element? element.innerHTML: null;
+            }, TABLE_FIELDS.replace("ROWINDEX", i).replace("COLINDEX", j));
+            j!==8?console.log('Column'+j+':',ColVal.replace(/\n/g,'')):console.log('Column'+j+':',ColVal.replace('<br>','').replace(/\s/g,''));
+        }
+
         let RadioSelector = DATE_RADIO_SELECTOR.replace("INDEX", i);
         await page.click(RadioSelector);
         await page.waitForSelector(ACTIVE_ADDRESS_TAB_SELECTOR); //go to address page
@@ -74,21 +83,29 @@ async function run(aVin) {
         //await page.waitForNavigation({timeout:30001});
         //await page.waitFor(1000);
 
-        // let filepath = 'screenshots/dc' + i + '.png';
-        // await page.screenshot({ path: filepath });
-        //await page.waitForSelector('#searchCriteria > canthappen');
-
         let emailAddr = await page.evaluate(() => {
             let element = document.getElementsByName('eMail');
             return element? element[0].value: null;
           });
+        console.log('emailAddr:',emailAddr);
 
-        console.log('emailAddr',emailAddr);
-        //emailAddr&&console.log('emailAddrV',emailAddr[0].value);
-        await page.waitFor(1000);
+        let Title = await page.evaluate((sel) => {
+            let element = document.querySelector(sel);
+            return element? element.options[element.selectedIndex].text: null;
+          },TITLE_FIELD_DROPDOWN); //dropdown
+        if(Title===null){
+            Title = await page.evaluate((sel) => {
+                let element = document.querySelector(sel);
+                return element? element.innerHTML: null;
+            },TITLE_FIELD_TEXT); //just text
+        }
+            console.log('Title:',Title);
+
+        //await page.waitFor(500);
         await page.waitForSelector(BACK_TO_LIST_TAB_SELECTOR,{timeout:30003}); //go back to list
+        //await page.waitFor(500);
         await page.click(BACK_TO_LIST_TAB_SELECTOR);    
-        await page.waitForNavigation({timeout:30002});
+        await page.waitForSelector(TABLE_ROW);
 
     }
 
@@ -97,10 +114,13 @@ async function run(aVin) {
     // browser.close();
 }
 
-run(aVin);
+LogIn();
 
 //if any awaits time out, program ends
 process.on('unhandledRejection', (err) => { 
     console.error(err)
     process.exit(1)
   })
+
+// let filepath = 'screenshots/dc' + i + '.png';
+// await page.screenshot({ path: filepath });
